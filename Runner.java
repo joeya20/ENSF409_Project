41514f4bd6b8e01dcc88cfd425957105840e308b
joeya20 <56373482+joeya20@users.noreply.github.com>
2021-04-04 18:27:35 -0600

    Added Comments

--- Runner.java
+++ Runner.java
@@ -14,6 +14,10 @@ public class Runner {
     private final static String PASSWORD = "Deanjo_19";
     private final static String URL = "jdbc:mysql://localhost/inventory";
 
+    /**
+     * Responsible for the IO and flow of the program. 
+     * Utilizes the MySQLService Class to access the database and the CombinationFinder Class to determine a valid combination.
+     */
     public static void main(String[] args) {
         CombinationFinder solver = null;
         MySQLService db = null;
@@ -40,35 +44,37 @@ public class Runner {
                 System.out.println("4. lamp");
                 category = input.nextLine().toLowerCase().trim();  //store as lowercase for comparisons
 
-                if(category.equals("exit")) {
+                if(category.equals("exit")) {   //end program if exit was inputted
                     return;
                 }
-                else if(isValidCategory(category)) {
+                else if(isValidCategory(category)) {    //break if category is valid
                     break;
                 }
-                else {
+                else {  //else print invalid category and reprompt an input
                     System.out.print("Invalid furniture Category! ");
                 }
             }
 
+            //get all valid types of the input category
             types = getCategoryTypes(category);
 
             //get furniture type
             while(true) {
                 System.out.println("Enter one of the following "+ category +" types or \"exit\" to end program: ");
 
+                //print all valid types of the category as possibilites
                 for (int i = 0; i < types.length; i++) {                    
                     System.out.println(String.valueOf(i+1) + ". " + types[i]);
                 }
                 type = input.nextLine().toLowerCase().trim();
 
-                if(type.equals("exit")) {
+                if(type.equals("exit")) {   //end program if exit was inputted
                     return;
                 }
-                else if(Arrays.asList(types).contains(type)) {
+                else if(Arrays.asList(types).contains(type)) {  //break if the input was one of the valid furniture types
                     break;
                 }
-                else {
+                else {  //print invalid type and reprompt an input
                     System.out.print("Invalid furniture type! ");
                 }
             }
@@ -78,38 +84,41 @@ public class Runner {
                 System.out.println("Enter the desired amount of " + type + " " + category + "(s) or \"exit\" to end program: ");
                 String placeholder = input.nextLine().trim();
                 
-                if(placeholder.equals("exit")) {
+                if(placeholder.equals("exit")) {    //end program if exit was inputted
                     return;
                 }
-                else if(isValidAmount(placeholder)) {
+                else if(isValidAmount(placeholder)) {   //break if the input was a valid amount
                     amount = Integer.parseInt(placeholder);
                     break;
                 }
-                else {
+                else {  //print invalid amount and reprompt an input
                     System.out.print("Invalid furniture type! ");
                 }
             }
 
-            //process
-            solver = new CombinationFinder(db.getData(category), type, amount);
-            solver.solve();
-            InventoryEntity[] results = solver.getRemovedItems();
+            //instantiate a CombinationFinder object using data from the database and user input
+            solver = new CombinationFinder(db.getData(category), type, amount); 
+            solver.solve(); //find a valid combination
+            InventoryEntity[] results = solver.getRemovedItems();   //get the Items that fulfill the order, returns null for no solution
 
-            if(results == null) {
+            if(results == null) {   //if no solution was found
                 String[] manu = null;
                 int i = 0;
-                if(category.equals("chair")) {
-                    manu = db.getManu()[0];
+
+                if(category.equals("chair")) {  //get valid chair manufacturers
+                    manu = db.getManu()[0]; 
                 }
-                else if(category.equals("desk")){
+                else if(category.equals("desk")){ //get valid desk manufacturers
                     manu = db.getManu()[1];
                 }
-                else if(category.equals("lamp")) {
+                else if(category.equals("lamp")) {  //get valid lamp manufacturers
                     manu = db.getManu()[2];
                 }
-                else {
+                else {  //get valid filing manufacturers
                     manu = db.getManu()[3];
                 }
+
+                // output error message and end program
                 System.out.print("Order cannot be fulfilled based on current inventory. Suggested manufacturers are ");
                 for (i = 0; i < manu.length-1; i++) {
                     System.out.print(manu[i] + ", ");
@@ -117,9 +126,19 @@ public class Runner {
                 System.out.println(manu[i] + ".");
                 return;
             }
+
+            //remove the rows from the database if a solution was found
             db.updateDatabases(category, Arrays.asList(results));
             
-            //output
+            //output to command line
+            System.out.print("Purchase ");
+            int i = 0;
+            for (i = 0; i < results.length-1; i++) {
+                System.out.print(results[i].getId() + ", ");
+            }
+            System.out.println("and " + results[i].getId() + " for $" + solver.getBestPrice());
+
+            //Generate OrderForm.txt
             writer.write("Furniture Order Form");
             writer.newLine();
             writer.newLine();
@@ -136,7 +155,7 @@ public class Runner {
             writer.write("Item(s) Ordered:");
             writer.newLine();
 
-            for (int i = 0; i < results.length; i++) {
+            for (i = 0; i < results.length; i++) {
                 writer.write(results[i].getId());
                 writer.newLine();
             }
@@ -168,7 +187,7 @@ public class Runner {
      */
     public static boolean isValidCategory(String category) {
         if(category.equals("chair") || category.equals("desk")
-        || category.equals("filing") || category.equals("lamp")){
+        || category.equals("filing") || category.equals("lamp")) {
             return true;
         }
         return false;
